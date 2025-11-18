9a6ade4167cddba9ead708bc38b6a0dd2d6df9d8
Johan Backman <johan.backman@affirm.com>
2025-11-18 10:45:45 -0800

    Shutdown when idle

--- lib/ysc/application.ex
+++ lib/ysc/application.ex
@@ -11,25 +11,32 @@ defmodule Ysc.Application do
       config: %{metadata: [:file, :line]}
     })
 
-    children = [
-      # Start the Telemetry supervisor
-      YscWeb.Telemetry,
-      # Start the Ecto repository
-      Ysc.Repo,
-      # Start the PubSub system
-      {Phoenix.PubSub, name: Ysc.PubSub},
-      # Start DNS cluster to cluster the app
-      {DNSCluster, query: Application.get_env(:ysc, :dns_cluster_query) || :ignore},
-      # Start Finch
-      {Finch, name: Ysc.Finch},
-      # Start cache
-      {Cachex, name: :ysc_cache},
-      # Start the Endpoint (http/https)
-      YscWeb.Endpoint,
-      # Start a worker by calling: Ysc.Worker.start_link(arg)
-      # {Ysc.Worker, arg}
-      {Oban, Application.fetch_env!(:ysc, Oban)}
-    ]
+    # Add shutdown task for sandbox environment only
+    children =
+      [
+        # Start the Telemetry supervisor
+        YscWeb.Telemetry,
+        # Start the Ecto repository
+        Ysc.Repo,
+        # Start the PubSub system
+        {Phoenix.PubSub, name: Ysc.PubSub},
+        # Start DNS cluster to cluster the app
+        {DNSCluster, query: Application.get_env(:ysc, :dns_cluster_query) || :ignore},
+        # Start Finch
+        {Finch, name: Ysc.Finch},
+        # Start cache
+        {Cachex, name: :ysc_cache},
+        # Start the Endpoint (http/https)
+        YscWeb.Endpoint,
+        # Start a worker by calling: Ysc.Worker.start_link(arg)
+        # {Ysc.Worker, arg}
+        {Oban, Application.fetch_env!(:ysc, Oban)}
+      ] ++
+        if sandbox_environment?() do
+          [{Task, fn -> shutdown_when_inactive(:timer.minutes(10)) end}]
+        else
+          []
+        end
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
@@ -51,5 +58,22 @@ defmodule Ysc.Application do
   def config_change(changed, _new, removed) do
     YscWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  # Check if we're running in the sandbox environment
+  defp sandbox_environment? do
+    System.get_env("ENVIRONMENT", "development") |> String.downcase() == "sandbox"
+  end
+
+  # Shuts down the application if no active HTTP connections are found.
+  # This supports "scale to 0" on fly.io for the sandbox environment.
+  defp shutdown_when_inactive(every_ms) do
+    Process.sleep(every_ms)
+
+    if :ranch.procs(YscWeb.Endpoint.HTTP, :connections) == [] do
+      System.stop(0)
+    else
+      shutdown_when_inactive(every_ms)
+    end
   end
 end
