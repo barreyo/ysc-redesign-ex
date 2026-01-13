f701bcb9eccf18e224e69db425befd1bc419662b
Johan Backman <backman93@gmail.com>
2026-01-12 18:57:03 -0800

    Fix bad migration

--- priv/repo/migrations/20260108092911_create_booking_guests.exs
+++ priv/repo/migrations/20260108092911_create_booking_guests.exs
@@ -18,8 +18,8 @@ defmodule Ysc.Repo.Migrations.CreateBookingGuests do
       timestamps(type: :utc_datetime)
     end
 
-    create index(:booking_guests, [:booking_id], if_not_exists: true)
-    create index(:booking_guests, [:order_index], if_not_exists: true)
-    create index(:booking_guests, [:booking_id, :order_index], if_not_exists: true)
+    create index(:booking_guests, [:booking_id] )
+    create index(:booking_guests, [:order_index])
+    create index(:booking_guests, [:booking_id, :order_index])
   end
 end
